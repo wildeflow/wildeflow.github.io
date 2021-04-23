04bab86e89fd0410ba1f437f48efaba0efcd33d3
Florian Wilde <Crossmail@gmx.de>
2021-04-23 23:05:00 +0200

    optimize js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,8 @@
-/**
-* Template Name: iPortfolio - v3.1.0
-* Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
-  "use strict";
+/* global Swiper, AOS */
+// (() => {
+//   'use strict';
 
-  /**
-   * Easy selector helper function
-   */
+  /** jquery replacement functions */
   const select = (el, all = false) => {
     el = el.trim()
     if (all) {
@@ -19,9 +12,6 @@
     }
   }
 
-  /**
-   * Easy event listener function
-   */
   const on = (type, el, listener, all = false) => {
     let selectEl = select(el, all)
     if (selectEl) {
@@ -33,16 +23,11 @@
     }
   }
 
-  /**
-   * Easy on scroll event listener 
-   */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
 
-  /**
-   * Navbar links active state on scroll
-   */
+  /* Navbar links active state on scroll */
   let navbarlinks = select('#navbar .scrollto', true)
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
@@ -60,9 +45,7 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
-  /**
-   * Scrolls to an element with header offset
-   */
+  /* Scrolls to an element with header offset */
   const scrollto = (el) => {
     let elementPos = select(el).offsetTop
     window.scrollTo({
@@ -71,38 +54,31 @@
     })
   }
 
-  /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
+  /* Back to top button */
+  let backtotopButton = select('.back-to-top')
+  if (backtotopButton) {
     const toggleBacktotop = () => {
       if (window.scrollY > 100) {
-        backtotop.classList.add('active')
+        backtotopButton.classList.add('active')
       } else {
-        backtotop.classList.remove('active')
+        backtotopButton.classList.remove('active')
       }
     }
     window.addEventListener('load', toggleBacktotop)
     onscroll(document, toggleBacktotop)
   }
 
-  /**
-   * Mobile nav toggle
-   */
+  /* Mobile nav toggle */
   on('click', '.mobile-nav-toggle', function(e) {
     select('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
   })
 
-  /**
-   * Scrool with ofset on links with a class name .scrollto
-   */
+  /* Scroll with offset on links with a class name .scrollto */
   on('click', '.scrollto', function(e) {
     if (select(this.hash)) {
       e.preventDefault()
-
       let body = select('body')
       if (body.classList.contains('mobile-nav-active')) {
         body.classList.remove('mobile-nav-active')
@@ -114,9 +90,7 @@
     }
   }, true)
 
-  /**
-   * Scroll with ofset on page load with hash links in the url
-   */
+  /* Scroll with offset on page load with hash links in the url */
   window.addEventListener('load', () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
@@ -125,9 +99,7 @@
     }
   });
 
-  /**
-   * Hero type effect
-   */
+  /* typewriter effect */
   const typed = select('.typed')
   if (typed) {
     let typed_strings = typed.getAttribute('data-typed-items')
@@ -141,26 +113,22 @@
     });
   }
 
-  /**
-   * Skills animation
-   */
-  let skilsContent = select('.skills-content');
-  if (skilsContent) {
+  /* skills loading animation */
+  let skillsContent = select('.skills-content');
+  if (skillsContent) {
     new Waypoint({
-      element: skilsContent,
+      element: skillsContent,
       offset: '80%',
       handler: function(direction) {
         let progress = select('.progress .progress-bar', true);
         progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
+          el.style.width = `${el.getAttribute('aria-valuenow')}%`
         });
       }
     })
   }
 
-  /**
-   * Porfolio isotope and filter
-   */
+  /* Portfolio isotope and filter */
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
@@ -188,16 +156,12 @@
 
   });
 
-  /**
-   * Initiate portfolio lightbox 
-   */
+  /* Initiate portfolio lightbox */
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
   });
 
-  /**
-   * Portfolio details slider
-   */
+  /* Portfolio details slider */
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
@@ -212,38 +176,7 @@
     }
   });
 
-  /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
-
-  /**
-   * Animation on scroll
-   */
+  /* Animation on scroll */
   window.addEventListener('load', () => {
     AOS.init({
       duration: 1000,
@@ -253,4 +186,4 @@
     })
   });
 
-})()
+// })()
